a73a1cd23206bc8fe9047198a7226112e47b4cc6
chak <none>
2001-02-19 05:33:11 +0100

    hipar: changeset 2001-02-19 04:33:11 by chak
    - c2hs-config.in: generated code needs -package lang for compilation
      and linking
    
    - toplevel/Version.hs (versnum): 0.8.3 "Gentle Moon"
    
    darcs-hash:20010219043311-8368b-61157eff48645cd23da55a4d411eefd8805b6a3e

--- hipar/c2hs/toplevel/Version.hs
+++ hipar/c2hs/toplevel/Version.hs
@@ -4,11 +4,11 @@ where
 -- version number is major.minor.patchlvl; don't change the format of the
 -- `versnum' line as it is `grep'ed for by a Makefile
 --
-idstr      = "$Id: Version.hs,v 1.34 2001/02/12 06:34:40 chak Exp $"
+idstr      = "$Id: Version.hs,v 1.35 2001/02/19 04:33:13 chak Exp $"
 name       = "C->Haskell Compiler"
-versnum    = "0.8.2"
+versnum    = "0.8.3"
 versnick   = "\"Gentle Moon\""
-date	   = "12 Feb 2000"
+date	   = "19 Feb 2000"
 version    = name ++ ", version " ++ versnum ++ " " ++ versnick ++ ", " ++ date
 copyright  = "Copyright (c) [1999..2001] Manuel M. T. Chakravarty"
 disclaimer = "This software is distributed under the \
