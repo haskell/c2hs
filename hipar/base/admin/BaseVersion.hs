d48fee35648ea4090884ac28818b7efc483e20fb
chak <none>
2001-04-29 14:23:45 +0200

    hipar: changeset 2001-04-29 12:23:45 by chak
    - ../configure.in: Adapted to support ghc 5.x and removed support for
      ghc 3.x
    
    - admin/BaseVersion.hs (versnum): 0.22.3
    
    darcs-hash:20010429122345-8368b-1f4fa36d10d5c6bab37307dfe723a0e8fc6800c8

--- hipar/base/admin/BaseVersion.hs
+++ hipar/base/admin/BaseVersion.hs
@@ -4,12 +4,12 @@ where
 -- version number is major.minor.patchlvl; don't change the format of the
 -- `versnum' line as it is `grep'ed for by a Makefile
 --
-idstr      = "$Id: BaseVersion.hs,v 1.31 2000/10/05 07:51:28 chak Exp $"
+idstr      = "$Id: BaseVersion.hs,v 1.32 2001/04/29 12:23:46 chak Exp $"
 name       = "Compiler Toolkit"
-versnum    = "0.22.2"
-date	   = "02 Oct 2000"
+versnum    = "0.22.3"
+date	   = "21 Apr 2001"
 version    = name ++ ", version " ++ versnum ++ ", " ++ date
-copyright  = "Copyright (c) [1995..2000] Manuel M. T. Chakravarty"
+copyright  = "Copyright (c) [1995..2001] Manuel M. T. Chakravarty"
 disclaimer = "This software is distributed under the \
 	     \terms of the GNU Public Licence.\n\
 	     \NO WARRANTY WHATSOEVER IS PROVIDED. \
