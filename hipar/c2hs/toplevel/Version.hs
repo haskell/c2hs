77139b27603e200fcb663ddf6815d7790d2cf774
chak <none>
2001-05-11 10:32:12 +0200

    hipar: changeset 2001-05-11 08:32:12 by chak
    - c2hs-config.in: Patch from Jens-Ulrik Petersen <juhp@01.246.ne.jp>
      fixes $sys variable setting
    
    - toplevel/Version.hs (versnum): 0.9.4
    
    darcs-hash:20010511083212-8368b-1c91eadcb4c766cc09430cb533092a561a7cd155

--- hipar/c2hs/toplevel/Version.hs
+++ hipar/c2hs/toplevel/Version.hs
@@ -4,11 +4,11 @@ where
 -- version number is major.minor.patchlvl; don't change the format of the
 -- `versnum' line as it is `grep'ed for by a Makefile
 --
-idstr      = "$Id: Version.hs,v 1.39 2001/05/05 08:48:45 chak Exp $"
+idstr      = "$Id: Version.hs,v 1.40 2001/05/11 08:32:15 chak Exp $"
 name       = "C->Haskell Compiler"
-versnum    = "0.9.3"
+versnum    = "0.9.4"
 versnick   = "\"Blue Ginger\""
-date	   = "05 May 2001"
+date	   = "11 May 2001"
 version    = name ++ ", version " ++ versnum ++ " " ++ versnick ++ ", " ++ date
 copyright  = "Copyright (c) [1999..2001] Manuel M. T. Chakravarty"
 disclaimer = "This software is distributed under the \
