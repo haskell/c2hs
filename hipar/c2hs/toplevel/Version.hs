27062e03fd54a4dedf42661612877b2b7b00470c
chak <none>
2002-03-03 08:05:03 +0100

    hipar: changeset 2002-03-03 07:05:03 by chak
    - configure.in: Package handling fix by Jens Petersen
    
    - toplevel/Version.hs (versnum): 0.10.9
    
    darcs-hash:20020303070503-8368b-a13e5ef2d044f352823a38fcfb2bbb5eba7cf496

--- hipar/c2hs/toplevel/Version.hs
+++ hipar/c2hs/toplevel/Version.hs
@@ -4,11 +4,11 @@ where
 -- version number is major.minor.patchlvl; don't change the format of the
 -- `versnum' line as it is `grep'ed for by a Makefile
 --
-idstr      = "$Id: Version.hs,v 1.58 2002/02/25 06:19:56 chak Exp $"
+idstr      = "$Id: Version.hs,v 1.59 2002/03/03 07:05:04 chak Exp $"
 name       = "C->Haskell Compiler"
-versnum    = "0.10.8"
+versnum    = "0.10.9"
 versnick   = "\"Altocumulus Stratiformis Perlucidus Undulatus\""
-date	   = "25 Feb 2002"
+date	   = "03 Mar 2002"
 version    = name ++ ", version " ++ versnum ++ " " ++ versnick ++ ", " ++ date
 copyright  = "Copyright (c) [1999..2002] Manuel M T Chakravarty"
 disclaimer = "This software is distributed under the \
