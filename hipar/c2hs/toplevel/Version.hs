8fde5d8ce2e7ed1c92a567b45eb9d695bd8a290e
chak <none>
2002-07-06 11:59:40 +0200

    hipar: changeset 2002-07-06 09:59:40 by chak
    wibble
    
    darcs-hash:20020706095940-8368b-99d884df22b5a7c133db53ee6b0319f2daff6db1

--- hipar/c2hs/toplevel/Version.hs
+++ hipar/c2hs/toplevel/Version.hs
@@ -4,11 +4,11 @@ where
 -- version number is major.minor.patchlvl; don't change the format of the
 -- `versnum' line as it is `grep'ed for by a Makefile
 --
-idstr      = "$Id: Version.hs,v 1.63 2002/05/16 13:58:34 chak Exp $"
+idstr      = "$Id: Version.hs,v 1.64 2002/07/06 09:59:40 chak Exp $"
 name       = "C->Haskell Compiler"
-versnum    = "0.10.14"
+versnum    = "0.10.15"
 versnick   = "\"Altocumulus Stratiformis Perlucidus Undulatus\""
-date	   = "16 May 2002"
+date	   = "6 Jul 2002"
 version    = name ++ ", version " ++ versnum ++ " " ++ versnick ++ ", " ++ date
 copyright  = "Copyright (c) [1999..2002] Manuel M T Chakravarty"
 disclaimer = "This software is distributed under the \
