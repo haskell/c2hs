2a4a5e6c54e187e46fb075502c164400835fa297
chak <none>
2002-09-07 06:21:34 +0200

    hipar: changeset 2002-09-07 04:21:34 by chak
    - configure.in: fixed REQUIRES_HASKELL for ghc
    
    - toplevel/Version.hs (versnum): 0.10.17
    
    darcs-hash:20020907042134-8368b-869f4463a66997e6041500b6a1b6201c7f78d1b7

--- hipar/c2hs/toplevel/Version.hs
+++ hipar/c2hs/toplevel/Version.hs
@@ -4,11 +4,11 @@ where
 -- version number is major.minor.patchlvl; don't change the format of the
 -- `versnum' line as it is `grep'ed for by a Makefile
 --
-idstr      = "$Id: Version.hs,v 1.65 2002/09/06 06:34:13 chak Exp $"
+idstr      = "$Id: Version.hs,v 1.66 2002/09/07 04:21:35 chak Exp $"
 name       = "C->Haskell Compiler"
-versnum    = "0.10.16"
+versnum    = "0.10.17"
 versnick   = "\"Altocumulus Stratiformis Perlucidus Undulatus\""
-date	   = "06 Sep 2002"
+date	   = "07 Sep 2002"
 version    = name ++ ", version " ++ versnum ++ " " ++ versnick ++ ", " ++ date
 copyright  = "Copyright (c) [1999..2002] Manuel M T Chakravarty"
 disclaimer = "This software is distributed under the \
