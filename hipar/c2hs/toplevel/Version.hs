381f76235e7245b2031bf083e2ddaf35fe15372b
chak <none>
2003-10-19 09:07:04 +0200

    hipar: changeset 2003-10-19 07:07:04 by chak
    2003-06-10 Manuel M T Chakravarty <chak@cse.unsw.edu.au>        *
    toplevel/Version.hs (versnum): 0.11.5 2003-05-30 Jens Petersen
    <petersen@redhat.com>  * configure.in: Search for compiler named HC
    too. 2003-05-30 Jens Petersen <petersen@redhat.com>        *
    c2hs.spec.in (Version): Set directly.         (Release): Ditto.
    (%prep): Quieten setup.         (%build): Use configure macro.
    (%install): Use makeinstall macro.      (%post): Use _bindir.
    (%files): Make root own files. Use _bindir, _libdir and _mandir.
    
    darcs-hash:20031019070704-8368b-9896caf2ce2235d79297f34b26e5835bebd11521

--- hipar/c2hs/toplevel/Version.hs
+++ hipar/c2hs/toplevel/Version.hs
@@ -4,11 +4,11 @@ where
 -- version number is major.minor.patchlvl; don't change the format of the
 -- `versnum' line as it is `grep'ed for by a Makefile
 --
-idstr      = "$Id: Version.hs,v 1.69 2003/05/22 04:15:03 chak Exp $"
+idstr      = "$Id: Version.hs,v 1.70 2003/10/19 07:07:05 chak Exp $"
 name       = "C->Haskell Compiler"
-versnum    = "0.11.4"
+versnum    = "0.11.5"
 versnick   = "\"Powder Snow\""
-date	   = "22 May 2003"
+date	   = "10 Jun 2003"
 version    = name ++ ", version " ++ versnum ++ " " ++ versnick ++ ", " ++ date
 copyright  = "Copyright (c) [1999..2003] Manuel M T Chakravarty"
 disclaimer = "This software is distributed under the \
