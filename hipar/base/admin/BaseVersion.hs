b2ec338d0efc370cef4df2d3ffa55b5ce04d0795
chak <none>
2003-10-19 12:45:27 +0200

    hipar: changeset 2003-10-19 10:45:27 by chak
    - sysdep/Makefile: Posix (and hence, `runPiped') support disabled, as
      `runPiped' (1) isn't really used at the moment, (2) should be
      rewritten anyway, and (3) breaks with GHC 6.3 as the signature of
      `forkProcess' changed.
    
    - admin/BaseVersion.hs (versnum): 0.26.0
    
    darcs-hash:20031019104527-8368b-297302b0f80898253b79d9824d4d88f666e7066c

--- hipar/base/admin/BaseVersion.hs
+++ hipar/base/admin/BaseVersion.hs
@@ -4,10 +4,10 @@ where
 -- version number is major.minor.patchlvl; don't change the format of the
 -- `versnum' line as it is `grep'ed for by a Makefile
 --
-idstr      = "$Id: BaseVersion.hs,v 1.42 2003/10/19 07:07:48 chak Exp $"
+idstr      = "$Id: BaseVersion.hs,v 1.43 2003/10/19 10:45:27 chak Exp $"
 name       = "Compiler Toolkit"
-versnum    = "0.25.1"
-date	   = "10 Jun 2003"
+versnum    = "0.26.0"
+date	   = "19 Oct 2003"
 version    = name ++ ", version " ++ versnum ++ ", " ++ date
 copyright  = "Copyright (c) [1995..2003] Manuel M T Chakravarty"
 disclaimer = "This software is distributed under the \
