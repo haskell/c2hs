2056c85dce69c5e02b78828a4e1ca636532ad45f
chak <none>
2005-05-18 05:04:02 +0200

    hipar: changeset 2005-05-18 03:04:02 by chak
    - Fixed cyclic dependency between building libctk.a and
      base/base.build.conf
    
    - Fixed install targets
    
    - Introduced CABAL variables to the make process (instead of testing
      for version numbers of GHC)
    
    darcs-hash:20050518030402-8368b-38d9043b9ff3ba4dc7bec79755c26e2bdbaae220

--- hipar/base/admin/BaseVersion.hs
+++ hipar/base/admin/BaseVersion.hs
@@ -4,12 +4,12 @@ where
 -- version number is major.minor.patchlvl; don't change the format of the
 -- `versnum' line as it is `grep'ed for by a Makefile
 --
-idstr      = "$Id: BaseVersion.hs,v 1.43 2003/10/19 10:45:27 chak Exp $"
+idstr      = "$Id: BaseVersion.hs,v 1.44 2005/05/18 03:04:03 chak Exp $"
 name       = "Compiler Toolkit"
-versnum    = "0.26.0"
-date	   = "19 Oct 2003"
+versnum    = "0.27.0"
+date	   = "18 May 2005"
 version    = name ++ ", version " ++ versnum ++ ", " ++ date
-copyright  = "Copyright (c) [1995..2003] Manuel M T Chakravarty"
+copyright  = "Copyright (c) [1995..2005] Manuel M T Chakravarty"
 disclaimer = "This software is distributed under the \
 	     \terms of the GNU Public Licence.\n\
 	     \NO WARRANTY WHATSOEVER IS PROVIDED. \
--- hipar/c2hs/toplevel/Version.hs
+++ hipar/c2hs/toplevel/Version.hs
@@ -4,11 +4,11 @@ where
 -- version number is major.minor.patchlvl; don't change the format of the
 -- `versnum' line as it is `grep'ed for by a Makefile
 --
-idstr      = "$Id: Version.hs,v 1.78 2005/03/14 00:26:59 chak Exp $"
+idstr      = "$Id: Version.hs,v 1.79 2005/05/18 03:04:03 chak Exp $"
 name       = "C->Haskell Compiler"
-versnum    = "0.13.5"
+versnum    = "0.13.6"
 versnick   = "\"Pressing Forward\""
-date	   = "14 Mar 2005"
+date	   = "18 May 2005"
 version    = name ++ ", version " ++ versnum ++ " " ++ versnick ++ ", " ++ date
 copyright  = "Copyright (c) [1999..2005] Manuel M T Chakravarty"
 disclaimer = "This software is distributed under the \
