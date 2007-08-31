332b2c2268dfa37bd757841261383d7a6ae1bf19
Duncan Coutts <duncan.coutts@worc.ox.ac.uk>
2007-08-31 02:21:41 +0200

    Update version number, name and other package meta-data
    darcs-hash:20070831002141-9561d-ace400fc1905549d843e66e0ead4529c0dc9cca4

--- c2hs/toplevel/Version.hs
+++ c2hs/toplevel/Version.hs
@@ -6,10 +6,10 @@ import Data.Version (showVersion)
 
 name       = "C->Haskell Compiler"
 versnum    = Paths_c2hs.version
-versnick   = "Travelling Lightly"
-date	   = "28 Apr 2006"
+versnick   = "Rainy Days"
+date	   = "31 Aug 2007"
 version    = name ++ ", version " ++ showVersion versnum ++ " " ++ versnick ++ ", " ++ date
-copyright  = "Copyright (c) [1999..2006] Manuel M T Chakravarty"
+copyright  = "Copyright (c) [1999..2007] Manuel M T Chakravarty"
 disclaimer = "This software is distributed under the \
 	     \terms of the GNU Public Licence.\n\
 	     \NO WARRANTY WHATSOEVER IS PROVIDED. \
