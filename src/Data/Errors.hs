c2acc789732cd523dec2633172ab10c6e6b61ee5
benedikt.huber <benedikt.huber@gmail.com>
2008-08-14 18:07:59 +0200

    Data.Errors: Use Language.C.Data.Error
    darcs-hash:20080814160759-c1f02-cca6a629238a40a1416462bd227fc9c0eaa6b34a

--- src/Data/Errors.hs
+++ src/Data/Errors.hs
@@ -34,13 +34,14 @@ module Data.Errors (
   --
   interr, todo,
   --
-  -- errors in the compiled program
+  -- errors in the compiled program (wrapper to Language.C Error type)
   --
-  ErrorLvl(..), Error, makeError, errorLvl, showError, errorAtPos
+  ErrorLevel(..), Error, makeError, errorLevel, showError, errorAtPos
 ) where
+import Language.C.Data.Error hiding (Error)
+import Language.C.Data.Position
 
-import Data.Position (Position(..), isInternalPos)
-
+type Error = CError
 
 -- internal errors
 -- ---------------
@@ -60,90 +61,15 @@ todo msg  = error ("Feature not yet implemented:\n"
                    ++ indentMultilineString 2 msg
                    ++ "\n")
 
-
--- errors in the compiled program
--- ------------------------------
-
--- | the higher the level of an error, the more critical it is
---
-data ErrorLvl = WarningErr              -- does not affect compilation
-              | ErrorErr                -- cannot generate code
-              | FatalErr                -- abort immediately
-              deriving (Eq, Ord)
-
-data Error = Error ErrorLvl Position [String]
-
--- note that the equality to on errors takes into account only the error level
--- and position (not the error text)
---
--- note that these comparisions are expensive (the positions contain the file
--- names as strings)
---
-instance Eq Error where
-  (Error lvl1 pos1 _) == (Error lvl2 pos2 _) = lvl1 == lvl2 && pos1 == pos2
-
-instance Ord Error where
-  (Error lvl1 pos1 _) <  (Error lvl2 pos2 _) = pos1 < pos2
-                                               || (pos1 == pos2 && lvl1 < lvl2)
-  e1                  <= e2                  = e1 < e2 || e1 == e2
-
-
 -- | produce an 'Error', given its level, position, and a list of lines of
 -- the error message that must not be empty
 --
-makeError :: ErrorLvl -> Position -> [String] -> Error
-makeError  = Error
+makeError :: ErrorLevel -> Position -> [String] -> Error
+makeError lvl pos msgs = CError $ ErrorInfo lvl pos msgs
 
--- | inquire the error level
---
-errorLvl                 :: Error -> ErrorLvl
-errorLvl (Error lvl _ _)  = lvl
-
--- | converts an error into a string using a fixed format
---
--- * the list of lines of the error message must not be empty
---
--- * the format is
---
--- >    <fname>:<row>: (column <col>) [<err lvl>]
--- >      >>> <line_1>
--- >      <line_2>
--- >        ...
--- >      <line_n>
---
--- * internal errors (identified by a special position value) are formatted as
---
--- >    INTERNAL ERROR!
--- >      >>> <line_1>
--- >      <line_2>
--- >        ...
--- >      <line_n>
---
-showError :: Error -> String
-showError (Error _   pos               (l:ls))  | isInternalPos pos =
-  "INTERNAL ERROR!\n"
-  ++ "  >>> " ++ l ++ "\n"
-  ++ (indentMultilineString 2 . unlines) ls
-showError (Error lvl (Position fname row col) (l:ls))  =
-  let
-    prefix = fname ++ ":" ++ show (row::Int) ++ ": "
-             ++ "(column "
-             ++ show (col::Int)
-             ++ ") ["
-             ++ showErrorLvl lvl
-             ++ "] "
-    showErrorLvl WarningErr = "WARNING"
-    showErrorLvl ErrorErr   = "ERROR"
-    showErrorLvl FatalErr   = "FATAL"
-  in
-  prefix ++ "\n"
-  ++ "  >>> " ++ l ++ "\n"
-  ++ (indentMultilineString 2 . unlines) ls
-showError (Error _  _                  []   )   = interr "Errors: showError:\
-                                                        \ Empty error message!"
 
 errorAtPos         :: Position -> [String] -> a
-errorAtPos pos msg  = (error . showError . makeError ErrorErr pos) msg
+errorAtPos pos msg  = (error . show . makeError LevelError pos) msg
 
 -- | indent the given multiline text by the given number of spaces
 --
