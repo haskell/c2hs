8c6ee67cb037d6ec8c854d6025b215cb7d7f4e72
Ian Ross <ian@skybluetrades.net>
2015-03-03 18:11:26 +0100

    Debug output

--- src/C2HS/Gen/Wrapper.hs
+++ src/C2HS/Gen/Wrapper.hs
@@ -62,24 +62,32 @@ genWrapper (Wrapper _ ofn _ _) = error $ "WRAPPER BORKED FOR " ++ ofn
 rename :: Ident -> CDeclarator a -> CDeclarator a
 rename ide (CDeclr _ dds str attrs n) = CDeclr (Just ide) dds str attrs n
 
-fixArgs :: [Bool] -> CDeclarator a -> CDeclarator a
+fixArgs :: Show a => [Bool] -> CDeclarator a -> CDeclarator a
 fixArgs args (CDeclr ide [fd] str attrs n) = CDeclr ide [fd'] str attrs n
   where fd' = fixFunArgs args fd
+fixArgs args cdecl = error $ "NON-EXHAUSTIVE PATTERN IN fixArgs\n" ++
+                     "args=" ++ show args ++ "\ncdecl=" ++ show cdecl
 
-fixFunArgs :: [Bool] -> CDerivedDeclarator a -> CDerivedDeclarator a
+fixFunArgs :: Show a => [Bool] -> CDerivedDeclarator a -> CDerivedDeclarator a
 fixFunArgs args (CFunDeclr (Right (adecls, flg)) attrs n) =
   CFunDeclr (Right (adecls', flg)) attrs n
   where adecls' = zipWith fixDecl args adecls
+fixFunArgs args cdecl = error $ "NON-EXHAUSTIVE PATTERN IN fixFunArgs\n" ++
+                     "args=" ++ show args ++ "\ncdecl=" ++ show cdecl
 
-fixDecl :: Bool -> CDeclaration a -> CDeclaration a
+fixDecl :: Show a => Bool -> CDeclaration a -> CDeclaration a
 fixDecl False d = d
 fixDecl True (CDecl specs [(Just decl, Nothing, Nothing)] n) =
   CDecl specs [(Just decl', Nothing, Nothing)] n
   where decl' = addPtr decl
+fixDecl arg cdecl = error $ "NON-EXHAUSTIVE PATTERN IN fixDecl\n" ++
+                    "arg=" ++ show arg ++ "\ncdecl=" ++ show cdecl
 
-addPtr :: CDeclarator a -> CDeclarator a
+addPtr :: Show a => CDeclarator a -> CDeclarator a
 addPtr (CDeclr ide [] cs attrs n) =
   CDeclr ide [CPtrDeclr [] n] cs attrs n
+addPtr cdecl = error $ "NON-EXHAUSTIVE PATTERN IN addPtr\n" ++
+               "cdecl=" ++ show cdecl
 
 callBody :: String -> [Bool] -> CDeclarator a -> CExpression a
 callBody fn args (CDeclr _ [fd] _ _ n) =
