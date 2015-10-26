33d9086b74b72e8bdf43f1af5b0d24d117dd3071
Ian Ross <iross@cadasta.org>
2015-10-26 20:49:23 +0100

    Version bump for 0.26.2

--- src/C2HS/Version.hs
+++ src/C2HS/Version.hs
@@ -10,7 +10,7 @@ versnum :: Version
 name       = "C->Haskell Compiler"
 versnum    = Paths_c2hs.version
 versnick   = "Budburst"
-date       = "4 April 2015"
+date       = "26 October 2015"
 version    = name ++ ", version " ++ showVersion versnum ++ " " ++ versnick ++ ", " ++ date
 copyright  = "Copyright (c) 1999-2007 Manuel M T Chakravarty\n"
           ++ "              2005-2008 Duncan Coutts\n"
